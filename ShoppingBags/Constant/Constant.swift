e004c518b27280bd20fa54d275b55f05eb069ae6
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-14 11:28:33 +0900

    [UI] 이미지, 색상 설정 추가

---
+++ ShoppingBags/Constant/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  ShoppingBags
+//
+//  Created by Jisoo Ham on 6/14/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum Colors {
+        static let orange = UIColor(named: "accentColor")
+        static let black = UIColor(named: "basicBlack")
+        static let white = UIColor(named: "basicWhite")
+        static let lightGray = UIColor(named: "GrayLight")
+        static let mediumGray = UIColor(named: "GrayMedium")
+        static let darkGray = UIColor(named: "GrayDark")
+    }
+}

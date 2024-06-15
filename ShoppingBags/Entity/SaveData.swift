717bc35bb23f5fe9c01604ec432c1a8480980c78
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-15 18:22:39 +0900

    [Feat] UserDefaults 저장 목록 enum화 및 데이터 CRUD 로직

---
+++ ShoppingBags/Entity/SaveData.swift
@@ -0,0 +1,15 @@
+//
+//  SaveData.swift
+//  ShoppingBags
+//
+//  Created by Jisoo Ham on 6/15/24.
+//
+
+import Foundation
+
+public enum SaveData: String {
+    case signupDate
+    case nickname
+    case profileImgTitle
+    case searchedText
+}
---
+++ ShoppingBags/UserDefaultsManager.swift
@@ -0,0 +1,41 @@
+//
+//  UserDefaultsManager.swift
+//  ShoppingBags
+//
+//  Created by Jisoo Ham on 6/15/24.
+//
+
+import UIKit
+
+public final class UserDefaultsManager {
+    public static let shared = UserDefaultsManager()
+
+    private init() {
+        
+    }
+    public func saveValueArray(
+        _ value: [String],
+        forKey key: SaveData
+    ) {
+        UserDefaults.standard.setValue(
+            value,
+            forKey: key.rawValue
+        )
+    }
+    public func saveValue(
+        _ value: Any,
+        forKey key: SaveData
+    ) {
+        UserDefaults.standard.setValue(
+            value,
+            forKey: key.rawValue
+        )
+    }
+    public func getValue<T>(forKey key: SaveData) -> T? {
+        return UserDefaults.standard.value(forKey: key.rawValue) as? T
+    }
+
+    public func removeValue(forKey key: SaveData) {
+        UserDefaults.standard.removeObject(forKey: key.rawValue)
+    }
+}

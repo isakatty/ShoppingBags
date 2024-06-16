06c05486aa9c24ff14dadbcc94f552666cbc6fd9
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-16 15:59:29 +0900

    [Feat] 데이터 모델에 맞춰 네트워크 통신 수정 및 CollectionView, Cell 생성, 라이브러리(SPM) 추가
    - 네트워크 통신을 통해 디코딩할 모델에 맞춰 데이터 받아올 수 있게 변경
    - Custom CollectionViewCell 추가
    - CollectionView Layout을 Compositional Layout으로 적용
    - 이미지 불러오기 위해 SPM을 통해 Kingfisher 라이브러리 추가

--- ShoppingBags/Search/SearchResult/SearchResultViewController.swift
+++ ShoppingBags/Search/SearchResult/SearchResultViewController.swift
@@ -11,6 +11,12 @@ import Alamofire
 
 public final class SearchResultViewController: UIViewController {
     public var searchedText: String?
+    private lazy var searchedResult: Search = Search(
+        total: 0,
+        start: 0,
+        display: 0,
+        items: []
+    )
     private var page: Int = 1
     private var sorting: SortedItem = .accuracy
     private let totalItemLabel: UILabel = {
@@ -20,6 +26,20 @@ public final class SearchResultViewController: UIViewController {
         // dummy Text
         label.text = "235,499개의 검색결과"
         return label
+    }()
+    private lazy var itemCollectionView: UICollectionView = {
+        let collection = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: collectionViewLayout()
+        )
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(
+            SearchedItemCollectionViewCell.self,
+            forCellWithReuseIdentifier: SearchedItemCollectionViewCell.identifier
+        )
+        collection.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        return collection
     }()
     
     public override func viewDidLoad() {
@@ -36,9 +56,8 @@ public final class SearchResultViewController: UIViewController {
             sorting: sorting
         )
     }
-    
     private func configureHierarchy() {
-        [totalItemLabel]
+        [totalItemLabel, itemCollectionView]
             .forEach { view.addSubview($0) }
     }
     private func configureLayout() {
@@ -48,6 +67,10 @@ public final class SearchResultViewController: UIViewController {
             make.top.trailing.equalTo(safeArea)
             make.leading.equalTo(safeArea).inset(12)
             make.height.equalTo(20)
+        }
+        itemCollectionView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalTo(safeArea)
+            make.top.equalTo(totalItemLabel.snp.bottom)
         }
     }
     private func configureNavigationBar() {
@@ -79,10 +102,14 @@ public final class SearchResultViewController: UIViewController {
             headers: header
         )
         .validate(statusCode: 200..<300)
-        .responseDecodable(of: Search.self) { response in
+        .responseDecodable(of: Search.self) { [weak self] response in
+            guard let self else { return }
             switch response.result {
             case .success(let value):
-                print(value)
+                self.searchedResult = value
+                self.totalItemLabel.text = 
+                "\(self.searchedResult.totalItems)개의 검색 결과"
+                self.itemCollectionView.reloadData()
             case .failure(let error):
                 // 재검색 유도
                 print(error)
@@ -122,6 +149,59 @@ public final class SearchResultViewController: UIViewController {
     }
     @objc private func customBackBtnTapped() {
         navigationController?.popViewController(animated: true)
+    }
+    private func collectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(0.5)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: item,
+            count: 2
+        )
+        group.interItemSpacing = .fixed(Constant.CollectionCell.spacing.rawValue)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = Constant.CollectionCell.spacing.rawValue
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: Constant.CollectionCell.spacing.rawValue,
+            bottom: 0,
+            trailing: Constant.CollectionCell.spacing.rawValue
+        )
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+}
+
+extension SearchResultViewController
+: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return searchedResult.items.count
+    }
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: SearchedItemCollectionViewCell.identifier,
+            for: indexPath
+        ) as? SearchedItemCollectionViewCell
+        else { return UICollectionViewCell() }
+        cell.configureUI(item: searchedResult.items[indexPath.item])
+        return cell
     }
     
 }
---
+++ ShoppingBags/Search/SearchResult/SearchedItemCollectionViewCell.swift
@@ -0,0 +1,127 @@
+//
+//  SearchedItemCollectionViewCell.swift
+//  ShoppingBags
+//
+//  Created by Jisoo Ham on 6/16/24.
+//
+
+import UIKit
+
+import Kingfisher
+
+public final class SearchedItemCollectionViewCell: UICollectionViewCell {
+    private let itemImgView: UIImageView = {
+        let img = UIImageView()
+        img.clipsToBounds = true
+        img.contentMode = .scaleToFill
+        img.layer.cornerRadius
+         = Constant.CornerRadius.collectionViewImg.rawValue
+        return img
+    }()
+    private lazy var mallNameLabel = makeLabel(
+        font: Constant.Font.light13,
+        color: Constant.Colors.lightGray,
+        lines: 1
+    )
+    private lazy var itemNameLabel = makeLabel(
+        font: Constant.Font.regular14,
+        color: Constant.Colors.black,
+        lines: 2
+    )
+    private lazy var itemPriceLabel = makeLabel(
+        font: Constant.Font.bold15,
+        color: Constant.Colors.black,
+        lines: 1
+    )
+    public let shoppingBtn: UIButton = {
+        var config = UIButton.Configuration.plain()
+        config.image = Constant.Images.likeUnselected
+        config.contentInsets = NSDirectionalEdgeInsets(
+            top: 5,
+            leading: 10,
+            bottom: 5,
+            trailing: 10
+        )
+        let imgConfig = UIImage.SymbolConfiguration(
+            font: Constant.Font.regular13
+        )
+        let btn = UIButton(configuration: config)
+        btn.backgroundColor = Constant.Colors.lightGray
+        btn.layer.cornerRadius
+        = Constant.CornerRadius.shoppingBagBg.rawValue
+        return btn
+    }()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHierarchy() {
+        [
+            itemImgView,
+            shoppingBtn,
+            mallNameLabel,
+            itemNameLabel,
+            itemPriceLabel
+        ]
+            .forEach { contentView.addSubview($0) }
+    }
+    private func configureLayout() {
+        itemImgView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(itemImgView.snp.width).multipliedBy(1.2)
+        }
+        shoppingBtn.snp.makeConstraints { make in
+            make.bottom.trailing.equalTo(itemImgView).inset(10)
+            make.width.height.equalTo(itemImgView.snp.width).multipliedBy(0.2)
+        }
+        
+        mallNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(itemImgView.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(15)
+        }
+        itemNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(mallNameLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview()
+        }
+        itemPriceLabel.snp.makeConstraints { make in
+            make.top.equalTo(itemNameLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(17)
+            make.bottom.lessThanOrEqualToSuperview()
+        }
+    }
+    
+    private func makeLabel(
+        font: UIFont,
+        color: UIColor?,
+        lines: Int
+    ) -> UILabel {
+        let label = UILabel()
+        label.font = font
+        label.textColor = color
+        label.numberOfLines = lines
+        return label
+    }
+    public func configureUI(item: Item) {
+        mallNameLabel.text = item.mallName
+        itemNameLabel.text = item.formattedItemName
+        itemPriceLabel.text = item.formattedPrice
+        
+        guard let url = URL(string: item.itemImage) else { return }
+        itemImgView.kf.setImage(with: url)
+    }
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        itemImgView.image = nil
+    }
+}

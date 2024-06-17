5ea723929fd1746f3aa6c37ebe2709e9246a5e3f
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-06-17 16:12:17 +0900

    [Chore] Constant에 프로필 이미지 추가 및 접근제어 수정

--- ShoppingBags/Constant/Constant.swift
+++ ShoppingBags/Constant/Constant.swift
@@ -28,6 +28,25 @@ enum Constant {
         static let likeUnselected = UIImage(named: "like_unselected")
     }
     
+    enum ProfileImages: String, CaseIterable {
+        case profile0 = "profile_0"
+        case profile1 = "profile_1"
+        case profile2 = "profile_2"
+        case profile3 = "profile_3"
+        case profile4 = "profile_4"
+        case profile5 = "profile_5"
+        case profile6 = "profile_6"
+        case profile7 = "profile_7"
+        case profile8 = "profile_8"
+        case profile9 = "profile_9"
+        case profile10 = "profile_10"
+        case profile11 = "profile_11"
+        
+        var profileImg: UIImage? {
+            return UIImage(named: self.rawValue)
+        }
+    }
+    
     enum SystemImages {
         static let leftChevron = UIImage(systemName: "chevron.left")
         static let rightChevron = UIImage(systemName: "chevron.right")
--- ShoppingBags/Entity/TextFieldStatus.swift
+++ ShoppingBags/Entity/TextFieldStatus.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-enum TextFieldStatus: String, CaseIterable {
+public enum TextFieldStatus: String, CaseIterable {
     case includeIcons = "닉네임에 @,#,$,%는 포함할 수 없어요"
     case failedTextCondition = "2글자 이상 10글자 미만으로 설정해주세요"
     case includedNumbers = "닉네임에 숫자는 포함할 수 없어요"
